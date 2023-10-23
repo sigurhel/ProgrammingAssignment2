fe0718c5c1c382d9d1aab40dc32db0eba9ec3423
sigurhel <sigurhel@hotmail.com>
2023-10-23 11:11:47 +0200

    Solution for the programming assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Wrapper for a matrix
+## set(y), set the wrapped matrix to y
+## get(), returns the wrapped matrix
+## getinv(), returns the cached inverse of wrapped matrix
+## setinv(matrixInverse), set the cached matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mInv <- NULL
+    set <- function(y) {
+        xMatrix <<- y
+        mInv <<- NULL
+    }
+    get <- function() xMatrix
+    setinv <- function(matrixInverse) mInv <<- matrixInverse
+    getinv <- function() mInv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix contained in a makeCacheMatrix object 
+## and caches it.
+## If the inverse was already calculated, returned the cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    mInv <- x$getinv()
+    if(!is.null(mInv)) {
+        message("getting cached data")
+        return(mInv)
+    }
+    data <- x$get()
+    mInv <- solve(data, ...)
+    x$setinv(mInv)
+    mInv
 }
